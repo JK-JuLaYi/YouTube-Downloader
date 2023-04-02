e5bee10203849696a2373f58f430aed3d64787b8
JK-JuLaYi <113028494+JK-JuLaYi@users.noreply.github.com>
2023-04-02 18:08:11 +0530

    Update youtube_video_downloader.py

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -1,10 +1,10 @@
 import os
 
 import pytube
-import pandas as pd
+
 
 # specify the YouTube video URL
-# video_url = "https://www.youtube.com/embed/S06jHWa7Brk"
+video_url = "https://www.youtube.com/embed/<code>"
 from pytube.exceptions import VideoUnavailable
 
 
@@ -23,27 +23,5 @@ def video_downloader(url,folder):
     else:
         print(f'{yt.title} already available')
 
-def list_videos():
-    # replace 'file_path' with the path of the Excel workbook you want to read
-    file_path = "F:\\AI\\Speak\\urls.xlsx"
 
-    # read all sheets of the Excel workbook into a dictionary of data frames
-    sheets_dict = pd.read_excel(file_path , sheet_name=None, header=None)
-    # print(sheets_dict)
-
-    # loop through the dictionary and print the name and contents of each sheet
-    for sheet_name, sheet_data in sheets_dict.items():
-        new_folder = os.getcwd()+'\\Videos\\'+sheet_name
-        if not os.path.exists(new_folder):
-            os.mkdir(new_folder)
-        for links in sheet_data.iloc[:,0]:
-            # print(links)
-            try:
-                video_downloader(links,new_folder)
-            except VideoUnavailable:
-                pass
-
-        # print(sheet_data)
-
-
-list_videos()
+video_downloader(video_url, outputFolder)
